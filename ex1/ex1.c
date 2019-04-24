ab1f8a04e9c633ba854d9e9755eac5e04561311c
steelworx <eileencuevas62@gmail.com>
2019-04-24 16:03:00 -0400

    completed exercise 1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,6 +1,7 @@
 // Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
+// Answer: both the child and parent processes have their own copy of x, and manage to change their own copy of x during execution
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +10,24 @@
 int main(void)
 {
     // Your code here
+
+    int var_to_change = 100; // initialize this int as 100
+
+    printf("Starting var: %d\n", var_to_change);
+
+    int pid = fork();
+    // child process starts executing here
+
+    if (pid == 0) // child variable satisfies this branch
+    {
+        var_to_change = 50; // change the variable in some way
+        printf("Child process with pid: %d, your variable is now %d\n", pid, var_to_change);
+    }
+    else
+    {
+        var_to_change = 25; // change the variable in some way
+        printf("Parent process with pid: %d, your variable is now %d\n", pid, var_to_change);
+    }
 
     return 0;
 }
